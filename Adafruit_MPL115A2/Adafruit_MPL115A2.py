336c40a54cbd4dfb04cec4249bd8e32e01e85d91
Jared Loosli <jloosli@gmail.com>
2013-02-18 21:57:17 -0700

    Changed to 8 bit reads

--- Adafruit_MPL115A2/Adafruit_MPL115A2.py
+++ Adafruit_MPL115A2/Adafruit_MPL115A2.py
@@ -98,20 +98,20 @@ class MPL115A2 :
     self._mpl115a2_b2 = 0
     self._mpl115a2_c12 = 0
 
-    self._mpl115a2_a0 = self.i2c.readS16(self.__MPL115A2_REGISTER_A0_COEFF_MSB)   # INT16
-    self._mpl115a2_a0 = self._mpl115a2_a0 << 8 | self.i2c.readS16(self.__MPL115A2_REGISTER_B1_COEFF_LSB)   # INT16
-    self._mpl115a2_b1 = self.i2c.readS16(self.__MPL115A2_REGISTER_B1_COEFF_MSB)   # INT16
-    self._mpl115a2_b1 = self._mpl115a2_b1 << 8 | self.i2c.readS16(self.__MPL115A2_REGISTER_B1_COEFF_LSB)   # INT16
-    self._mpl115a2_b2 = self.i2c.readS16(self.__MPL115A2_REGISTER_B2_COEFF_MSB)   # INT16
-    self._mpl115a2_b2 = self._mpl115a2_b2 << 8 | self.i2c.readS16(self.__MPL115A2_REGISTER_B2_COEFF_LSB)   # INT16
-    self._mpl115a2_c12 = self.i2c.readS16(self.__MPL115A2_REGISTER_C12_COEFF_MSB)   # INT16
-    self._mpl115a2_c12 = self._mpl115a2_b2 << 8 | self.i2c.readS16(self.__MPL115A2_REGISTER_C12_COEFF_LSB)   # INT16
+    self._mpl115a2_a0 = self.i2c.readS8(self.__MPL115A2_REGISTER_A0_COEFF_MSB)   # INT16
+    self._mpl115a2_a0 = self._mpl115a2_a0 << 8 | self.i2c.readS8(self.__MPL115A2_REGISTER_B1_COEFF_LSB)   # INT16
+    self._mpl115a2_b1 = self.i2c.readS8(self.__MPL115A2_REGISTER_B1_COEFF_MSB)   # INT16
+    self._mpl115a2_b1 = self._mpl115a2_b1 << 8 | self.i2c.readS8(self.__MPL115A2_REGISTER_B1_COEFF_LSB)   # INT16
+    self._mpl115a2_b2 = self.i2c.readS8(self.__MPL115A2_REGISTER_B2_COEFF_MSB)   # INT16
+    self._mpl115a2_b2 = self._mpl115a2_b2 << 8 | self.i2c.readS8(self.__MPL115A2_REGISTER_B2_COEFF_LSB)   # INT16
+    self._mpl115a2_c12 = self.i2c.readS8(self.__MPL115A2_REGISTER_C12_COEFF_MSB)   # INT16
+    self._mpl115a2_c12 = self._mpl115a2_b2 << 8 | self.i2c.readS8(self.__MPL115A2_REGISTER_C12_COEFF_LSB)   # INT16
 
     self._mpl115a2_a0 /= 8
     self._mpl115a2_b1 /= 8192
     self._mpl115a2_b2 /= 16384
     self._mpl115a2_a0 /= 4194304.0
-    
+
     if (self.debug):
       self.showCalibrationData()
 
