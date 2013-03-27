8e55401ef2c0cdb113d1e5fa2204395236f06613
Jared Loosli <jloosli@gmail.com>
2013-03-26 23:20:52 -0600

    Added steps

--- incite/testadc.py
+++ incite/testadc.py
@@ -2,7 +2,22 @@
 
 
 from Adafruit_ADS1x15 import ADS1x15
-import imp
+import time, math
+import datetime
+from Adafruit_8x8 import EightByEight
+import Adafruit_LEDBackpack
+
+# ===========================================================================
+# 8x8 Pixel Example
+# ===========================================================================
+grid = EightByEight(address=0x70)
+led = Adafruit_LEDBackpack.LEDBackpack(0x70)
+led.setBrightness=1
+
+print "Press CTRL+Z to exit"
+
+display=0
+
 
 
 # ============================================================================
@@ -36,3 +51,19 @@ if ADS_Current == ADS1015:
 else:
   # For ADS1115 at max range (+/-6.144V) 1-bit = 0.1875mV (16-bit values)
   print "Channel 1 = %.3f V" % (result * 0.0001875)
+
+while 1:
+  result = adc.readADCSingleEnded(1)
+  val = result * 0.0001875
+  steps = math.floor(val / 6.144 * 64)
+  print "Channel 0 = %.3f V" % (result * 0.0001875)
+  print "Steps = %d" % (steps)
+  grid.clear()
+  i=0
+  for x in range(0, 8):
+    for y in range(0, 8):
+      if i < steps:
+        grid.setPixel(x, y)
+      i += 1
+
+  time.sleep(1)
